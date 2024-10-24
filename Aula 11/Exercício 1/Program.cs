4f359e918cbfef9cbec0f6fd50199007f7cd7543
LusquetaMendAlm <mendoncalucas2772@gmail.com>
2024-10-24 15:19:05 -0300

    Aula 11

---
+++ Aula 11/Exercício 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercício_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new();
+            Console.WriteLine("Digite a altura e em seguida a largura do retângulo.");
+            retangulo.altura = double.Parse(Console.ReadLine());
+            retangulo.largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Para esse retângulo, as medidas são:");
+            retangulo.calcArea();
+            retangulo.calcPerimetro();
+        }
+    }
+}
---
+++ Aula 11/Exercício 1/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Retangulo
+    {
+        //Os atributos e métodos precisam ser "internal" ou "public" para que as outras classes os acessem
+        internal double altura;
+        internal double largura;
+
+        internal void calcArea()
+        {
+            Console.WriteLine("Área: " + (altura * largura));
+            
+        }
+        internal void calcPerimetro()
+        {
+            Console.WriteLine("Perimêtro: " + (2 * ( altura + largura)));
+        }
+    }
+}
