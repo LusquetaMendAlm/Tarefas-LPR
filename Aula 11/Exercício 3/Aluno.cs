65348f4f81826a32de876caade851f0eddd13aa4
LusquetaMendAlm <mendoncalucas2772@gmail.com>
2024-10-29 22:28:04 -0300

    Aula 11

---
+++ Aula 11/Exercício 3/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_3
+{
+    internal class Aluno
+    {
+        internal int matricula;
+        internal string nome;
+        internal int prova1;
+        internal int prova2;
+        internal int trabalho;
+        internal double mediaNota;
+        internal double falta;
+
+        internal double media()
+        {
+            mediaNota = (((2.5 * (prova1 + prova2)) + (2 * trabalho)) / 2);
+
+            return mediaNota;
+        }
+
+        internal double final()
+        {
+            if (mediaNota < 60)
+            {
+
+                return 0;
+            }
+            else
+            {
+                falta = 100 - mediaNota;
+
+                return falta;
+            }
+        }
+    }
+}
---
+++ Aula 11/Exercício 3/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exercício_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha;
+            bool continuar = true;
+
+            Aluno aluno = new();
+            Console.WriteLine("Digite a matrícula do aluno: ");
+            aluno.matricula = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do aluno:");
+            aluno.nome = Console.ReadLine();
+            Console.WriteLine("Digite a nota da primeira prova:");
+            aluno.prova1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota da segunda prova:");
+            aluno.prova2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do trabalho:");
+            aluno.trabalho = int.Parse(Console.ReadLine());
+
+            while (continuar == true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Digite um dos números:");
+                Console.WriteLine("1. Fazer a média das notas;");
+                Console.WriteLine("2. Verificar quanto de nota precisa na final;");
+                Console.WriteLine("3. Sair.");
+
+                escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.WriteLine("Média: " + aluno.media());
+                        break;
+                    case 2:
+                        Console.WriteLine("A nota necessária é " + aluno.final());
+                        break;
+                    case 3:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Esse número não pode");
+                        break;
+                }
+            }
+        }
+    }
+}
