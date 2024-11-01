2013ee8588850b7755f2810642ae057d853dd89d
LusquetaMendAlm <mendoncalucas2772@gmail.com>
2024-11-01 16:58:49 -0300

    Aula Get&Set

---
+++ Getter&Setters/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter_Setters
+{
+    internal class Aluno
+    {
+        //POR ENCAPSULAMENTO , MEUS ATRIBUTOS DEVEM SER PRIVADOS
+        string nome;
+        //int turma;
+        public int Turma { get; set;}
+        double mediaNotas;
+
+        //CONSTRUTOR= MÉTODO ESPECIAL PÚBLICO
+        public Aluno(string nome, int turma)
+        {
+            this.nome = nome;
+            this.turma = turma;
+        }
+
+        ////GET = MÉTODO ESPECIAL
+        //public string getNome()
+        //{
+        //return nome; 
+        //}
+
+        ////SET = MÉTODO ESPECIAL
+        //public void setNome(string nome)
+        //{
+        //    this.nome = nome;
+        //}
+
+        //MÉTODO GETSET = MÉTODO ESPECIAL
+        public string Nome
+        {
+            get  { return nome; }
+            set  { nome = value; }
+        }
+    }
+}
---
+++ Getter&Setters/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Getter_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSTANCIANDO MEU OBJETO
+            var aluno1 = new Aluno("Lucas", 34);
+
+            //PILAR DO ENCAPSULAMENTO: NÃO PODE ISSO!!!
+            //aluno1.nome = "Luquinha";
+            //Console.WriteLine(aluno1.nome);
+
+            //ACESSO AO ATRIBUTO POR MÉTODO GET
+            //Console.WriteLine(aluno1.getNome());
+            //var x = aluno1.getNome();
+
+            ////ALTERAÇÃO DE ATRIBUTO POR MÉTODO SET
+            //aluno1.setNome("Luquinha");
+            //Console.WriteLine(aluno1.getNome()); //TESTE
+            //string y = "Lusqueta";
+            //aluno1.setNome(y);
+            //Console.WriteLine(aluno1.getNome()); //TESTE
+
+            //ACESSO AO ATRIBUTO POR MÉTODO GET
+            Console.WriteLine(aluno1.Nome);
+            var x = aluno1.Nome;
+
+            //ALTERAÇÃO DE ATRIBUTO POR MÉTODO SET
+            aluno1.Nome = "Lusquinha";
+            Console.WriteLine(aluno1.Nome); //TESTE
+
+        }
+    }
+}
